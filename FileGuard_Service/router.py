c21d3b7f43bd836d0e48ceede8343d2db54ec785
Himayu18 <himayudhoke18@gmail.com>
2026-02-25 18:50:55 +0530

    some thing has been fixed

--- FileGuard_Service/router.py
+++ FileGuard_Service/router.py
@@ -6,11 +6,16 @@ from FileGuard_Service.detect_file import detect_files
 
 router = APIRouter()
 
+from fastapi import HTTPException
+
 @router.post("/detect")
 def scan_pdf(file_path: str = Depends(validated_pdf)):
     try:
         result = detect_files(file_path)
         return result
+    except Exception as e:
+        print("Detection error:", e)
+        raise HTTPException(status_code=400, detail=str(e))
     finally:
         if os.path.exists(file_path):
             os.remove(file_path)
